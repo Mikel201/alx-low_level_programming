723114321927ba72f6421f3b7d2ba45984ff9a6d
Mikel201 <michealiyobosa298@gmail.com>
2022-03-12 16:56:06 -0800

    getting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *My hello, program
+ *My hello, print task
  *Return: return 1 after program execution
  */
 int main(void)
 {
-	printf("with proper grammer, but thr outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
 
